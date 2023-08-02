6a36da86d01ec30bc3c2942a34bf67345acf2093
Williams Salazar <93990745+williamsjdev@users.noreply.github.com>
2023-08-01 21:51:31 -0400

    Add files via upload

---
+++ 44-PROYECTO-CRM-CRUD-REST/js/editarcliente.js
@@ -0,0 +1,58 @@
+import { obtenerCliente, editarCliente } from './API.js';
+import { mostrarAlerta, validar } from './funciones.js';
+
+(function() {
+
+    // Campos del formulario
+    const nombreInput = document.querySelector('#nombre');
+    const emailInput = document.querySelector('#email');
+    const telefonoInput = document.querySelector('#telefono');
+    const empresaInput = document.querySelector('#empresa');
+    const idInput = document.querySelector('#id');
+
+    document.addEventListener('DOMContentLoaded', async () => {
+        const parametrosURL = new URLSearchParams(window.location.search);
+
+        const idCliente = parseInt( parametrosURL.get('id') );
+
+        const cliente = await obtenerCliente(idCliente);
+        mostrarCliente(cliente);
+
+        // Submit al formulario
+        const formulario = document.querySelector('#formulario');
+        formulario.addEventListener('submit', validarCliente);
+    });
+
+    function mostrarCliente(cliente) {
+        const { nombre, email, telefono, empresa, id } = cliente;
+
+        nombreInput.value = nombre;
+        emailInput.value = email;
+        telefonoInput.value = telefono;
+        empresaInput.value = empresa;
+        idInput.value = id;
+    }
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        const cliente = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: parseInt(idInput.value)
+        }
+
+        console.log(cliente)
+
+        if ( validar (cliente) ){
+            // Mostrar mensajes
+            mostrarAlerta('Todos los campos son obligatorios');
+            return;
+        }
+
+        // Reescribe el objeto
+        editarCliente(cliente); 
+    }
+})();
